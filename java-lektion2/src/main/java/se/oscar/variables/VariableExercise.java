23192193885ba2fd8158ae122603b66e45eeefe4
Mojujo <oscar.tornqvist01@gmail.com>
2024-09-05 12:42:02 +0200

    Variables

--- java-lektion2/src/main/java/se/oscar/variables/VariableExercise.java
+++ java-lektion2/src/main/java/se/oscar/variables/VariableExercise.java
@@ -2,6 +2,12 @@ package se.oscar.variables;
 
 public class VariableExercise {
     public static void main(String[] args) {
+        String name = "Oscar";
+        int age = 23;
+        boolean isStudent = true;
 
+        System.out.println("name = " + name);
+        System.out.println("age = " + age);
+        System.out.println("isStudent = " + isStudent);
     }
 }
