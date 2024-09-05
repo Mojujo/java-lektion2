9cd8252d684365dc40df1c9b5814ee40bd232085
Mojujo <oscar.tornqvist01@gmail.com>
2024-09-05 11:36:15 +0200

    Add files via upload

---
+++ java-lektion2/src/main/java/se/oscar/loops/ArrayLoopExercise.java
@@ -0,0 +1,10 @@
+package se.oscar.loops;
+
+public class ArrayLoopExercise {
+    public static void main(String[] args) {
+        int[]arr = new int[] {1,2,3,4,5};
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+}
---
+++ java-lektion2/src/main/java/se/oscar/variables/VariableExercise.java
@@ -0,0 +1,7 @@
+package se.oscar.variables;
+
+public class VariableExercise {
+    public static void main(String[] args) {
+
+    }
+}
