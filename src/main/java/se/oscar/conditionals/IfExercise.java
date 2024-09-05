eb83e1892987c5628b17eb382e0cb6900700319c
Mojujo <oscar.tornqvist01@gmail.com>
2024-09-05 15:13:29 +0200

    15.12

---
+++ src/main/java/se/oscar/conditionals/IfExercise.java
@@ -0,0 +1,12 @@
+package se.oscar.conditionals;
+
+public class IfExercise {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+
+        if (a < b) {
+            System.out.println("A är större än B");
+        }
+    }
+}
